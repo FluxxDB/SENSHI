21752d03087b3483a71a5cc3d5a40bcb3348ab8d
PomboDev <21963363+PomboDev@users.noreply.github.com>
2023-07-22 02:13:07 -0300

    WIP: Entity setup

--- src/server/services/player/player-service.lua
+++ src/server/services/player/player-service.lua
@@ -3,48 +3,52 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 local CharacterService = require(Services["character-service"])
-local SpawnParamsTypes = require(ReplicatedStorage.types["spawn-params"])
-local Structs = require(ReplicatedStorage.Shared.structs)
+local SpawnParamsTypes = require(ReplicatedStorage.types["SpawnParams"])
+local Structs = require(ReplicatedStorage.Shared.modules.structs)
 local PlayerService = {}
 
-local entity = Structs.entity
-local transform = Structs.transform
 local spawnLocation = Workspace:WaitForChild("Map"):WaitForChild("Spawn Locations"):GetChildren()
 local defaultSpawnParams: SpawnParamsTypes.TSpawnParams = {
-    rigType = "Default",
-    position = spawnLocation[1].Position + Vector3.new(0, 5, 0)
+	rigType = "Default",
+	position = spawnLocation[1].Position + Vector3.new(0, 5, 0),
 }
 local entityIds = {} -- ( playerObject: EntityID )
 local profiles = {} -- ( EntityID: Profile )
 
 local function initPlayer(player)
-    -- TODO: Create folders and load profiles
-    local playerEntityId, playerEntity = entity.CreateStruct()
-    entityIds[player] = playerEntityId
+	local playerEntityId = Structs.entity:insert({
+		transformId = Structs.transform:insert({}),
+		statusId = Structs.status:insert({}),
+		-- TODO: Add Profile Struct (?)
+	})
+	entityIds[player] = playerEntityId
+	-- TODO: Create folders and load profiles
 
-    -- local character = CharacterService:spawnCharacter(player, nil, defaultSpawnParams) -- Fix nil and "Default" to corresponding data values
-    -- local humanoid = character:WaitForChild("Humanoid")
-    -- humanoid.Died:Connect(function()
-    --     task.delay(5, function()
-    --         initPlayer(player) -- Temporary respawn solution
-    --     end) 
-    -- end)
+	-- local character = CharacterService:spawnCharacter(player, nil, defaultSpawnParams) -- Fix nil and "Default" to corresponding data values
+	-- local humanoid = character:WaitForChild("Humanoid")
+	-- humanoid.Died:Connect(function()
+	--     task.delay(5, function()
+	--         initPlayer(player) -- Temporary respawn solution
+	--     end)
+	-- end)
 end
 
 local function clearPlayer(player)
-    local profile = table.find(profiles, player)
-    if not profile then return end
+	local profile = table.find(profiles, player)
+	if not profile then
+		return
+	end
 
-    table.remove(profiles, profile)
+	table.remove(profiles, profile)
 end
 
 function PlayerService:onInit()
-    print("Ayo")
+	print("Ayo")
 end
 
 function PlayerService:onStart()
-    Players.PlayerAdded:Connect(initPlayer)
-    Players.PlayerRemoving:Connect(clearPlayer)
+	Players.PlayerAdded:Connect(initPlayer)
+	Players.PlayerRemoving:Connect(clearPlayer)
 end
 
 return PlayerService
